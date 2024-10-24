a82c068087bab1fb5f069c9b4b666cf4aaa878b6
vitor852 <vitor.dc@hotmail.com>
2024-10-24 14:59:21 -0300

    feature: about us section

---
+++ src/app/_components/about-us.tsx
@@ -0,0 +1,14 @@
+const AboutUs = () => {
+  return (
+    <section className="h-screen bg-white">
+      <div className="flex items-center flex-grow py-10 text-black">
+        <div className="w-full flex flex-col">
+          <p className="text-3xl font-bold">1,200</p>
+          <span>Hectore total area</span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
