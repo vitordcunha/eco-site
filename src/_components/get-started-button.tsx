c6c4298d5163bb1b53ebe09fa544589c08856e3b
vitor852 <vitor.dc@hotmail.com>
2024-10-24 14:58:22 -0300

    feature: get started button

---
+++ src/_components/get-started-button.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { motion, Variants } from "framer-motion";
+import { ArrowUpRight } from "lucide-react";
+
+const GetStartedButton = () => {
+  const variants: Variants = {
+    animate: {
+      height: "52px",
+      width: "182px",
+      right: -1,
+      transition: { duration: 0.2 },
+    },
+  };
+
+  return (
+    <motion.button
+      className="flex bg-white rounded-full text-sm font-semibold items-center gap-10 h-[50px] w-[180px] relative text-black hover:text-white"
+      initial={false}
+      whileHover="animate"
+    >
+      <span className="whitespace-nowrap absolute left-4 z-20 transition-all delay-75">
+        Get Started
+      </span>
+
+      <motion.div
+        className="bg-blue-600 text-white rounded-full p-2 absolute right-1 z-10 flex justify-end items-center"
+        variants={variants}
+        layout
+      >
+        <ArrowUpRight />
+      </motion.div>
+    </motion.button>
+  );
+};
+
+export default GetStartedButton;
