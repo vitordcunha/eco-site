fba4e7ab4600b69c97e3cab28fb0c863323c2956
vitor852 <vitor.dc@hotmail.com>
2024-10-25 14:31:53 -0300

    feature: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,57 @@
 import Home from "./_components/home";
 import AboutUs from "./_components/about-us";
+import {
+  IconBrandGithub,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+} from "@tabler/icons-react";
+import { FileText } from "lucide-react";
+import { FloatingDock } from "@/_components/ui/floating-dock";
+import Services from "./_components/services";
+import Benefits from "./_components/benefits";
 
 export default function Site() {
   return (
-    <div className="">
+    <div className="relative">
       <Home />
       <AboutUs />
+      <Services />
+      <Benefits />
+
+      <div className="fixed bottom-3 z-30 left-1/2 translate-x-[-50%]">
+        <FloatingDock items={links} />
+      </div>
     </div>
   );
 }
+
+const links = [
+  {
+    title: "GitHub",
+    icon: (
+      <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: "#",
+  },
+  {
+    title: "Linkedin",
+    icon: (
+      <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: "#",
+  },
+  {
+    title: "Instagram",
+    icon: (
+      <IconBrandInstagram className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: "#",
+  },
+  {
+    title: "Resume",
+    icon: (
+      <FileText className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+    ),
+    href: "#",
+  },
+];
